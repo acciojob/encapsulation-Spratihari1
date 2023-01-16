d334e91de69ee4da853bac64d47df0faddc095a0
sandeep pratihari <spratihari1@gmail.com>
2023-01-16 12:01:24 +0530

    sol 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
 public static void main(String[] args){
     RWOnly obj= new RWOnly();
-    obj.writeOnly ("sandeep");
-    obj.readOnly ();
+    obj.setName ("sandeep");
+    obj.getName ();
     //'name' has private access in 'com.driver.RWOnly'
 
 
